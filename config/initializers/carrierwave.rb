30940638daa59122e53823b25cbf0de1fbd42b3b
Tate Kaneshige <tatekaneshige@dhcp-168-105-83-42.wireless.manoa.hawaii.edu>
2014-01-05 16:07:51 -1000

    fixing aws mistake

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,7 @@ CarrierWave.configure do |config|
       :aws_secret_access_key  => ENV['AWS_SECRET_KEY']        # required
       
     }
-    config.fog_directory  = ENV['ENV_BUCKET']                 # required
+    config.fog_directory  = ENV['AWS_BUCKET']                 # required
 
     config.storage = :fog
 
